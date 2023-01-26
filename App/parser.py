59ca95fbe43afab568e8de9353a5780a0cb08e2a
Renat Skosarev <skosarrrev@gmail.com>
2023-01-26 22:09:54 +0500

    update table parser

--- App/parser.py
+++ App/parser.py
@@ -7,12 +7,11 @@ from database import ScheduleTable
 class TableParser:
     def __init__(self, file_path: str):
         self.wb = load_workbook(file_path)
-        # self.sheet = self.wb[self.wb.sheetnames[0]]
         self.sheet = self.wb['1 неделя ']
         self.schedule_db = ScheduleTable()
 
     def parse(self):
-        days = {'понедельник': '23.01', 'вторник': '24.01', 'среда': '25.01', 'четверг': '26.01', 'пятница': '27.01'}
+        days = {'понедельник': '23.01', 'вторник': '24.01', 'среда': '25.01', 'четверг': '26.01', 'пятница': '27.01'}  # TODO update
 
         for clas in range(3, 76, 3):
             if clas == 39:
@@ -27,6 +26,7 @@ class TableParser:
                 for row in range(day, day + 5):
 
                     # fixes hidden row bug
+                    # TODO delete
                     if row == 28:
                         row = 29
 
@@ -35,24 +35,40 @@ class TableParser:
                         if str(i).startswith(self.sheet.cell(row, clas).coordinate):
                             merged = True
 
-                    number = row - day + 1 if row != 29 else 5
-                    subject = self.sheet.cell(row, clas).value
+                    number = row - day + 1 if row != 29 else 5  # TODO delete part 'if'
+                    subject1 = self.sheet.cell(row, clas).value
+                    try:
+                        teacher1 = subject1[subject1.index('(') + 1: subject1.index(')')]
+                        subject1 = subject1[:subject1.index('(')]
+                    except:
+                        teacher1 = None
+
+
                     subject2 = self.sheet.cell(row, clas + 1).value
+                    try:
+                        teacher2 = subject1[subject1.index('(') + 1: subject1.index(')')]
+                        subject2 = subject2[:subject1.index('(')]
+                    except:
+                        teacher2 = None
+
                     classroom = str(self.sheet.cell(row, clas + 2).value)
                     if classroom.endswith('.0'):
                         classroom = classroom[:-2]
+
                     clas_to_write = db_classes[weektable_classes.index(str(self.sheet.cell(2, clas).value))]
+                    clas_number = clas_to_write[:2]
+                    clas_profile = clas_to_write[2:]
 
                     if merged:
-                        self.schedule_db.save(date, number, subject, clas_to_write, 1, classroom)
-                        self.schedule_db.save(date, number, subject, clas_to_write, 2, classroom)
+                        self.schedule_db.save(date, number, subject1, teacher1, clas_number, clas_profile, 1, classroom)
+                        self.schedule_db.save(date, number, subject1, teacher1, clas_number, clas_profile, 2, classroom)
                     else:
                         classrooms = [classroom, classroom]
                         if '/' in classroom and classroom.lower() not in ['сп/з', 'с/з', 'а/з', 'акт/з', 'сп/зал',
                                                                           'с/зал', 'а/зал', 'акт/зал']:
                             classrooms = classroom.split('/')
-                        self.schedule_db.save(date, number, subject, clas_to_write, 1, classrooms[0])
-                        self.schedule_db.save(date, number, subject2, clas_to_write, 2, classrooms[1])
+                        self.schedule_db.save(date, number, subject1, teacher1, clas_number, clas_profile, 1, classrooms[0])
+                        self.schedule_db.save(date, number, subject2, teacher2, clas_number, clas_profile, 2, classrooms[1])
 
     def clear(self):
         self.schedule_db.clear()
