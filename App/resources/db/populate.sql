fa58d06fa6f6e8669a7f4e76efea6626de6b4942
Renat Skosarev <95104142+skosarevv@users.noreply.github.com>
2023-03-29 10:55:58 +0500

    Split DB init file to init and populate files

---
+++ App/resources/db/populate.sql
@@ -0,0 +1,35 @@
+INSERT INTO profile(name)
+VALUES ('био');
+INSERT INTO profile(name)
+VALUES ('геохим');
+INSERT INTO profile(name)
+VALUES ('исс1');
+INSERT INTO profile(name)
+VALUES ('исс2');
+INSERT INTO profile(name)
+VALUES ('линг1');
+INSERT INTO profile(name)
+VALUES ('линг2');
+INSERT INTO profile(name)
+VALUES ('мат');
+INSERT INTO profile(name)
+VALUES ('матэк');
+INSERT INTO profile(name)
+VALUES ('мед');
+INSERT INTO profile(name)
+VALUES ('фил');
+INSERT INTO profile(name)
+VALUES ('фм');
+INSERT INTO profile(name)
+VALUES ('эк');
+
+INSERT INTO profile(name)
+VALUES ('биохим');
+INSERT INTO profile(name)
+VALUES ('гео');
+INSERT INTO profile(name)
+VALUES ('исс');
+INSERT INTO profile(name)
+VALUES ('мат1');
+INSERT INTO profile(name)
+VALUES ('мат2');
