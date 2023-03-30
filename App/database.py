62ec0a51200a5c2c66f9dcb4466523320cfb35e6
Renat Skosarev <95104142+skosarevv@users.noreply.github.com>
2023-03-30 15:26:33 +0500

    Update naming in DB

--- App/database.py
+++ App/database.py
@@ -9,7 +9,7 @@ class UserTable:
         self.cur = self.con.cursor()
 
     def save_user(self, telegram_id, username, name, clas_number, clas_profile, group):
-        query = 'INSERT INTO user (telegram_id, username, name, clas_number, profile_id, "group")  VALUES (?, ?, ?, ?, (SELECT id FROM profile WHERE name = ?), ?);'
+        query = 'INSERT INTO user (telegram_id, username, name, class_number, class_profile, class_group)  VALUES (?, ?, ?, ?, (SELECT id FROM profile WHERE name = ?), ?);'
         self.cur.execute(query, (telegram_id, username, name, clas_number, clas_profile, group))
         self.con.commit()
 
@@ -30,7 +30,7 @@ class UserTable:
         self.con.commit()
 
     def update_user(self, id, telegram_id, username, name, clas_number, clas_profile, group):
-        query = 'UPDATE user SET telegram_id = ?, username = ?, name = ?, clas_number = ?, profile_id = (SELECT id from profile WHERE name = ?), "group" = ? WHERE id = ?;'
+        query = 'UPDATE user SET telegram_id = ?, username = ?, name = ?, class_number = ?, class_profile = (SELECT id from profile WHERE name = ?), class_group = ? WHERE id = ?;'
         self.cur.execute(query, (telegram_id, username, name, clas_number, clas_profile, group, id))
         self.con.commit()
 
@@ -45,41 +45,41 @@ class UserTable:
         self.con.commit()
 
     def get_class_number(self, telegram_id):
-        query = 'SELECT clas_number FROM user WHERE telegram_id = ?;'
+        query = 'SELECT class_number FROM user WHERE telegram_id = ?;'
         result = self.cur.execute(query, (telegram_id,)).fetchone()
         return result[0]
 
     def set_class_number(self, telegram_id, clas_number):
-        query = 'UPDATE user SET clas_number = ? WHERE telegram_id = ?;'
+        query = 'UPDATE user SET class_number = ? WHERE telegram_id = ?;'
         self.cur.execute(query, (clas_number, telegram_id))
         self.con.commit()
 
     def get_class_profile(self, telegram_id):
-        query = 'SELECT name FROM profile WHERE id = (SELECT profile_id FROM user WHERE telegram_id = ?);'
+        query = 'SELECT name FROM profile WHERE id = (SELECT class_profile FROM user WHERE telegram_id = ?);'
         result = self.cur.execute(query, (telegram_id,)).fetchone()
         return result[0]
 
     def set_class_profile(self, telegram_id, clas_profile):
-        query = 'UPDATE user SET profile_id = (SELECT id FROM profile WHERE name = ?) WHERE telegram_id = ?;'
+        query = 'UPDATE user SET class_profile = (SELECT id FROM profile WHERE name = ?) WHERE telegram_id = ?;'
         self.cur.execute(query, (clas_profile, telegram_id))
         self.con.commit()
 
     def get_group(self, telegram_id):
-        query = 'SELECT "group" FROM user WHERE telegram_id = ?;'
+        query = 'SELECT class_group FROM user WHERE telegram_id = ?;'
         result = self.cur.execute(query, (telegram_id,)).fetchone()
         return result[0]
 
     def set_group(self, telegram_id, group):
-        query = 'UPDATE user SET `group` = ? WHERE telegram_id = ?;'
+        query = 'UPDATE user SET class_group = ? WHERE telegram_id = ?;'
         self.cur.execute(query, (group, telegram_id))
         self.con.commit()
 
     def get_lastmessage(self, telegram_id):
-        query = 'SELECT last_message FROM user WHERE telegram_id = ?;'
+        query = 'SELECT prev_msg_time FROM user WHERE telegram_id = ?;'
         return self.cur.execute(query, (telegram_id,)).fetchone()[0]
 
     def set_lastmessage(self, telegram_id, last_message):
-        query = 'UPDATE user SET last_message = ? WHERE telegram_id = ?;'
+        query = 'UPDATE user SET prev_msg_time = ? WHERE telegram_id = ?;'
         self.cur.execute(query, (last_message, telegram_id))
         self.con.commit()
 
@@ -91,18 +91,18 @@ class ScheduleTable:
 
     def save(self, date: str, number: int, name: str, teacher: str, clas_number: int, clas_profile: str, group: int,
              classroom: str):
-        query = 'INSERT INTO schedule (date, number, name, teacher, clas_number, profile_id, "group", classroom) VALUES (?, ?, ?, ?, ?, (SELECT id FROM profile WHERE name = ?), ?, ?);'
+        query = 'INSERT INTO schedule (date, number, name, teacher, class_number, class_profile, class_group, classroom) VALUES (?, ?, ?, ?, ?, (SELECT id FROM profile WHERE name = ?), ?, ?);'
         self.cur.execute(query, (date, number, name, teacher, clas_number, clas_profile, group, classroom))
         self.con.commit()
 
     def get(self, date: str, clas_number: int, clas_profile: str):
         clas_profile = self.cur.execute('SELECT id FROM profile WHERE name = ?', (clas_profile,)).fetchone()[0]
-        query = 'SELECT * FROM schedule WHERE date = ? and clas_number = ? and profile_id = ? ORDER BY number;'
+        query = 'SELECT * FROM schedule WHERE date = ? and class_number = ? and class_profile = ? ORDER BY number;'
         return self.cur.execute(query, (date, clas_number, clas_profile)).fetchall()
 
     def get_for_group(self, date: str, clas_number: int, clas_profile: str, group: int):
         clas_profile = self.cur.execute('SELECT id FROM profile WHERE name = ?', (clas_profile,)).fetchone()[0]
-        query = 'SELECT * FROM schedule WHERE date = ? and clas_number = ? and profile_id = ? and "group" = ? ORDER BY number;'
+        query = 'SELECT * FROM schedule WHERE date = ? and class_number = ? and class_profile = ? and class_group = ? ORDER BY number;'
         return self.cur.execute(query, (date, clas_number, clas_profile, group)).fetchall()
 
     def get_for_teacher(self, date: str, teacher: str):
--- App/resources/db/init.sql
+++ App/resources/db/init.sql
@@ -10,28 +10,28 @@ CREATE TABLE profile
 
 CREATE TABLE user
 (
-    id           INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    telegram_id  INTEGER UNIQUE                    NOT NULL,
-    username     VARCHAR,
-    name         VARCHAR                           NOT NULL,
-    clas_number  INTEGER,
-    profile_id   INTEGER,
-    "group"      INTEGER,
-    state        INTEGER                           NOT NULL DEFAULT 0,
-    last_message VARCHAR                                    DEFAULT '2000-01-01 00:00:00.000000',
-    FOREIGN KEY (profile_id) REFERENCES profile (id)
+    id            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    telegram_id   INTEGER UNIQUE                    NOT NULL,
+    username      VARCHAR,
+    name          VARCHAR                           NOT NULL,
+    class_number  INTEGER,
+    class_profile INTEGER,
+    class_group   INTEGER,
+    state         INTEGER                           NOT NULL DEFAULT 0,
+    prev_msg_time VARCHAR                                    DEFAULT '2000-01-01 00:00:00.000000',
+    FOREIGN KEY (class_profile) REFERENCES profile (id)
 );
 
 CREATE TABLE schedule
 (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    date        VARCHAR                           NOT NULL,
-    number      INTEGER                           NOT NULL,
-    name        VARCHAR,
-    teacher     VARCHAR,
-    clas_number INTEGER                           NOT NULL,
-    profile_id  INTEGER                           NOT NULL,
-    "group"     INTEGER                           NOT NULL,
-    classroom   VARCHAR,
-    FOREIGN KEY (profile_id) REFERENCES profile (id)
+    id            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    date          VARCHAR                           NOT NULL,
+    number        INTEGER                           NOT NULL,
+    name          VARCHAR,
+    teacher       VARCHAR,
+    class_number  INTEGER                           NOT NULL,
+    class_profile INTEGER                           NOT NULL,
+    class_group   INTEGER                           NOT NULL,
+    classroom     VARCHAR,
+    FOREIGN KEY (class_profile) REFERENCES profile (id)
 );
