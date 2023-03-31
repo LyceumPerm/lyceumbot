b880e3aa406d02a8d8c2c86dd40a35acd37f9ab0
Renat Skosarev <95104142+skosarevv@users.noreply.github.com>
2023-03-31 10:49:06 +0500

    Update data to week 03.04-07.04

--- App/parser.py
+++ App/parser.py
@@ -20,6 +20,11 @@ class TableParser:
                 clas -= 1
 
             for day in range(4, 25, 5):
+
+                # лишняя строка в таблице после понедельника, поэтому инкрементируем day
+                if day > 4:
+                    day += 1
+
                 date = self.sheet.cell(day, 1).value.strftime('%d.%m')
 
                 for row in range(day, day + 5):
