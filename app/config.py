493525e3f87a5d26dd7fd9d1951a191290ebf6fc
Renat Skosarev <95104142+skosarevv@users.noreply.github.com>
2023-05-13 15:29:23 +0500

    Update data to week 15.05–19.05

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ SPAM_RESTRICTION = 2
 
 # data
 URL = 'https://docs.google.com/spreadsheets/d/1tGbeevMu_7_n_pKDFjH3cNFNigClVW3v'
-AVAILABLE_DAYS = ['08.05', '09.05', '10.05', '11.05', '12.05']
-CURRENT_TABLE = '10.05-12.05 неделя1'
+AVAILABLE_DAYS = ['15.05', '16.05', '17.05', '18.05', '19.05']
+CURRENT_TABLE = '15.05 - 19.05 2 неделя'
 
 # file path
 SCHEDULE_PATH = 'app/resources/schedule.xlsx'
